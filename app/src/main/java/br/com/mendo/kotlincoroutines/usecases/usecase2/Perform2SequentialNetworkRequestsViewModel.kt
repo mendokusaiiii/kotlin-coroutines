281eb487b899f6ff25c9e8b65a1e4b5c93842b3d
victor.orellana <victor.orellana@gmail.com>
2025-03-24 19:48:03 -0300

    feature: making a sequential network request with coroutines

--- app/src/main/java/br/com/mendo/kotlincoroutines/usecases/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
+++ app/src/main/java/br/com/mendo/kotlincoroutines/usecases/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
@@ -1,14 +1,28 @@
 package br.com.mendo.kotlincoroutines.usecases.usecase2
 
+import androidx.lifecycle.viewModelScope
 import br.com.mendo.kotlincoroutines.base.BaseViewModel
 import br.com.mendo.kotlincoroutines.mock.MockApi
 import br.com.mendo.kotlincoroutines.usecases.usecase2.callbacks.UiState
+import kotlinx.coroutines.launch
 
 class Perform2SequentialNetworkRequestsViewModel ( private val mockApi: MockApi
 ) : BaseViewModel<UiState>() {
 
     fun perform2SequentialNetworkRequest() {
+        uiState.value = UiState.Loading
 
+        viewModelScope.launch {
+            try {
+                val recentVersions = mockApi.getRecentAndroidVersions()
+                val mostRecentVersion = recentVersions.last()
+                val featuresOfMostRecentVersion = mockApi.getAndroidVersionFeatures(mostRecentVersion.apiLevel)
+                uiState.value = UiState.Success(featuresOfMostRecentVersion)
+            } catch (exception: Exception) {
+                uiState.value = UiState.Error("Network Request failed")
+            }
+
+        }
     }
 }
 
